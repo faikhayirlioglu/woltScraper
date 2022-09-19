b0aa4b5a20c2c62f82bbba93eacbd2b680262efb
Faik Hayirlioglu <faikhayirlioglu@gmail.com>
2022-09-19 22:41:37 +0300

    added code clarifications

--- main.py
+++ main.py
@@ -30,37 +30,47 @@ else:
 
 
 with open(title+"\list.txt","w") as f:
+    # Write company title
     f.write("TITLE:")
     f.write("\n"+title)
 
     for _ in categories:
+        # Write category name
         f.write("\n"*3+"CATEGORY:")
         f.write(categories[cat_index].string + "\n")
 
+        # Get the products of current category
         products = currentCat.find_all("p", class_="MenuItem-module__name___iqvnU")
 
         for i in products:
+            # Loop through product names and write it
             f.write("\n"+"  PRODUCT:")
             f.write(products[prod_index].string)
 
+            # Get current product
             currentProd = currentCat.find_all("p", class_="MenuItem-module__name___iqvnU")[prod_index]
+            # Check if there is description
             if currentProd.parent.find("p", class_="MenuItem-module__description___uzvuX"):
                 desc = currentProd.parent.find("p", class_="MenuItem-module__description___uzvuX").find(text=True, recursive=False).rstrip()
             else:
                 desc = "N/A"
 
+            # Write the description
             f.write("\n"+"      DESCRIPTION:")
             f.write(desc + "\n")
             
+            # Move to the next product index
             if prod_index+1 != len(products):
                 prod_index += 1
             else:
                 print("DONE")
 
+        # Move to the next category index
         if cat_index+1 != len(categories):
             cat_index += 1
             prod_index = 0
         else:
             print("DONE")
         
+        # Get current category for next loop
         currentCat = categories[cat_index].parent.parent.parent.parent
